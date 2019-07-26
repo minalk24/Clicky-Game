d3d055d1cfdbc762371e5a441c7765369db56a12
Minal Kharche <minal.berkeley@gmail.com>
2019-07-26 07:25:29 -0700

    added image card and temporary images list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import NavBar from "./components/Navbar";
 // import './App.css';
+import ImagesCard from "./components/Cards";
+import imagesCharacters from "./imagesList.json";
 
-class App extends Component{
+class App extends Component {
+  state = {
+    // Setting this.state.imagesList to the imagesList json array
+    imagesCharacters,
+    score: 0,
+    topScore: 0
+  }
+
   render() {
     return (
-      <NavBar />
+      <div>
+        <NavBar />
+        <div className="container-fluid">
+          <ImagesCard
+          // key={imagesCharacters.id}
+          />
+        </div>
+      </div>
     );
   }
 }
---
+++ src/components/Cards/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = (props) => {
+    return (
+        <img className="rounded-circle" type="button" 
+
+        style={{width: "200px", height: "200px"}}/>
+    );
+}
+
+export default Card;
