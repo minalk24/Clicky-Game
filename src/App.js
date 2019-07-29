5314447e4802a0748b74147a73607b4fad424557
Minal Kharche <minal.berkeley@gmail.com>
2019-07-28 22:04:20 -0700

    updated app.js with footer and functions to handle clickable picture

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import NavBar from "./components/Navbar";
 import ImagesCard from "./components/Cards";
 import imagesList from "./imagesList.json";
 // import './App.css';
+let footerCss = {
+  background : "#7da64c",
+  color: "#fff ",
+  height: "60px",
+  bottom: "0"
+};
 
 class App extends Component {
   state = {
     // Setting this.state.imagesList to the imagesList json array
     imagesList,
-    clickedImageIds : [],
+    clickedImageIds: [],
     score: 0,
     topScore: 0,
     message: "Click an image to begin!",
@@ -33,12 +39,12 @@ class App extends Component {
     let shuffledArray = this.handleShuffleArray(imagesList);
     this.setState({ imagesList: shuffledArray });
 
-    if (this.state.clickedImageIds.includes(id)){
+    if (this.state.clickedImageIds.includes(id)) {
       console.log('Game Over reset values ')
-      this.setState({ clickedImageIds: [], score: 0 , message:'You guessed incorrectly! '}); 
+      this.setState({ clickedImageIds: [], score: 0, message: 'You guessed incorrectly! ' });
       return;
 
-    }else { 
+    } else {
       this.setState({
         clickedImageIds: this.state.clickedImageIds.concat([id]),
         score: this.state.score + 1,
@@ -46,15 +52,15 @@ class App extends Component {
       });
       console.log("Score", this.state.score);
       console.log("TopScore", this.state.topScore);
-      
+
       if (this.state.score + 1 === 12) {
         // Shuffle Array
         this.handleShuffleArray(imagesList);
         this.setState({ imagesList: shuffledArray });
-        
+
         //Reset Game
         this.setState({
-          topScore: this.state.score + 1, 
+          topScore: this.state.score + 1,
           message: 'Congratulations!! You won click image to reset the game',
           //Reset the Game 
           score: 0,
@@ -63,29 +69,48 @@ class App extends Component {
 
       }
       else if (this.state.score + 1 > this.state.topScore) {
-        this.setState({ topScore: this.state.score + 1});
+        this.setState({ topScore: this.state.score + 1 });
       }
     }
   }
 
+  handleShuffleArray = imagesList => {
+    for (let i = imagesList.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [imagesList[i], imagesList[j]] = [imagesList[j], imagesList[i]];
+    }
+    return imagesList;
+  }
+
+
   render() {
     return (
       <div>
-      <NavBar 
-        score = {this.state.score}
-        topScore = {this.state.topScore}/>
-      <div className="container-fluid">
-      {this.state.imagesList.map(images => (
-        <ImagesCard
-          id={images.id}
-          key={images.id}
-          name={images.name}
-          image={images.image}
-          handleCardClicked={this.handleCardClicked}
-        />
-        ))}
+        <NavBar
+          score={this.state.score}
+          topScore={this.state.topScore}
+          message={this.state.message} />
+
+        <div className="container-fluid">
+          {this.state.imagesList.map(images => (
+            <ImagesCard
+              id={images.id}
+              key={images.id}
+              name={images.name}
+              image={images.image}
+              clickPicture={this.handleClickPicture}
+              animate={!this.state.score && this.state.topScore}
+            />
+          ))}
+        </div>
+
+        <footer className = "fixed-bottom" style = {footerCss}>
+          <center className="p-3">
+            <a href="https://github.com/minalk24/Clicky-Game" className="text-light" target="_blank" >Click for code </a>
+          </center>
+        </footer>
+
       </div>
-    </div>
     );
   }
 }
