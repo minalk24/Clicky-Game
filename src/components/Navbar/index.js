77b031bbadfccd45eb3c889d4ee44ca2fba79bb0
Minal Kharche <minal.berkeley@gmail.com>
2019-07-28 22:07:35 -0700

    updated with style

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ const Navbar = (props) => {
     return (
         <div>
             <nav className="navbar sticky-top navbar-dark">
-            <h2 classNamee="title col-4 text-center">React Memory Game</h2>
+            <h2 className="title col-4 text-center">React Memory Game</h2>
             <h4 className="col-4 text-center">Click an image to begin! </h4>
             <h2 className="text-light col-4 text-center">Score: {props.score} | Top Score: {props.topScore}</h2>
           </nav>
